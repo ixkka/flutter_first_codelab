fde6215956491c380ff4bfc13f18d8b3813bb3a0
Jan Francesca Logroño <janfrancesca.logrono.20@usjr.edu.ph>
2023-09-24 23:56:11 +0800

    center all widgets in the homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,15 +13,18 @@ class HomePage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            const Text('A random idea:'),
-            BigCard(wordPair: wordPair),
-            ElevatedButton(
-              onPressed: () => appState.getNext(),
-              child: const Text('Next'),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BigCard(wordPair: wordPair),
+              const SizedBox(height:10),
+              ElevatedButton(
+                onPressed: () => appState.getNext(),
+                child: const Text('Next'),
+              ),
+            ],
+          ),
         ),
       ),
     );
